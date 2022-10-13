852620fb5be6b25a7c8e8dc42daf79062e9a7e32
Virogu <3056184312@qq.com>
2022-10-13 11:17:48 +0800

    update XTimber.kt

--- xlog/src/main/java/com/virogu/xlog/XTimber.kt
+++ xlog/src/main/java/com/virogu/xlog/XTimber.kt
@@ -106,54 +106,49 @@ class XTimber {
         /***
          * 初始化xLog
          * @param context Context
+         * @param logLevel 日志文件记录级别，超过此级别的日志才会记录到文件中
+         * @param logMode 日志记录模式。[Xlog.AppednerModeSync]：同步记录 [Xlog.AppednerModeAsync]：异步记录
+         * @param cacheDays 日志缓存天数
          * @param cachePath log缓存目录 默认是 Android/data/packageName/files/xlog/cache
          * @param logPath log日志目录 默认是 Android/data/packageName/files/xlog
-         * @param namePrefix log日志文件前缀 默认是packageName，最终文件名格式为 “namePrefix_yyyyMMdd.xlog"
+         * @param logNameFilePrefix log日志文件前缀 默认是packageName，最终文件名格式为 “namePrefix_yyyyMMdd.xlog"
          */
         @SuppressLint("LogNotTimber")
+        @JvmOverloads
         @Synchronized
         fun initXLog(
             context: Context,
-            cachePath: String? = null,
-            logPath: String? = null,
-            namePrefix: String? = null,
+            @androidx.annotation.IntRange(
+                from = Xlog.LEVEL_ALL.toLong(),
+                to = Xlog.LEVEL_NONE.toLong()
+            )
+            logLevel: Int = Xlog.LEVEL_INFO,
+            @androidx.annotation.IntRange(from = 0, to = 1)
+            logMode: Int = Xlog.AppednerModeSync,
+            @androidx.annotation.IntRange(from = 0)
+            cacheDays: Int = 0,
+            cachePath: String = context.applicationContext.getExternalFilesDir("xlog/cache")?.path
+                ?: File(context.applicationContext.filesDir, "xlog/cache").path,
+            logPath: String = context.applicationContext.getExternalFilesDir("xlog")?.path
+                ?: File(context.applicationContext.filesDir, "xlog").path,
+            logNameFilePrefix: String = context.applicationContext.packageName,
         ) {
-            val mLogPath: String = logPath
-                ?: context.applicationContext.getExternalFilesDir("xlog")?.path
-                ?: File(context.applicationContext.filesDir, "xlog").path
-
-            File(mLogPath).apply {
-                if (!this.exists()) {
-                    this.mkdirs()
-                }
-            }
-
-            val mCachePath: String = cachePath
-                ?: context.applicationContext.getExternalFilesDir("xlog/cache")?.path
-                ?: File(context.applicationContext.filesDir, "xlog/cache").path
-
-            File(mCachePath).apply {
-                if (!this.exists()) {
-                    this.mkdirs()
-                }
-            }
-
-            val mLogFileName: String = namePrefix ?: context.applicationContext.packageName
-
+            File(logPath).mkdirs()
+            File(cachePath).mkdirs()
             val xlog = Xlog()
             Log.setLogImp(xlog)
             Log.setConsoleLogOpen(false)
             Log.appenderOpen(
-                Xlog.LEVEL_INFO,
-                Xlog.AppednerModeSync,
-                mCachePath,
-                mLogPath,
-                mLogFileName.replace(":", "_"),
-                0
+                logLevel,
+                logMode,
+                cachePath,
+                logPath,
+                logNameFilePrefix.replace(":", "_"),
+                cacheDays
             )
             android.util.Log.w(
                 "XTimber",
-                "XLog initialized,\n cachePath [$mCachePath],\n logPath [$mLogPath],\n logFileName [$mLogFileName]"
+                "XLog initialized,\n cachePath [$cachePath],\n logPath [$logPath],\n logFileName [$logNameFilePrefix]"
             )
         }
 
@@ -168,14 +163,29 @@ class XTimber {
 
     /**
      * Timber Tree
-     * @param isDebug 是否是Debug环境，不是Debug环境时，只显示INFO以上级别的日志
      */
-    class Tree(private val isDebug: Boolean) : Timber.DebugTree() {
+    class Tree : Timber.DebugTree {
+
+        private val logLevel: Int
+
+        //isDebug 是否是Debug环境，不是Debug环境时，logcat只显示INFO以上级别的日志
+        constructor(isDebug: Boolean) : this(android.util.Log.INFO)
+
+        // logcat只显示logLevel及以上级别的日志
+        constructor(
+            @androidx.annotation.IntRange(
+                from = android.util.Log.VERBOSE.toLong(),
+                to = android.util.Log.ASSERT.toLong()
+            )
+            logLevel: Int
+        ) {
+            this.logLevel = logLevel
+        }
 
         @SuppressLint("LogNotTimber")
         override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
             // 不是Debug模式，只显示INFO以上级别的日志
-            if (!isDebug && priority < android.util.Log.INFO) {
+            if (priority < logLevel) {
                 return
             }
             if (message.length < MAX_LOG_LENGTH) {
